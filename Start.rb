5289118ec795f99715b09b5ef66c255d27124f40
RemyMaucourt <remy.maucourt@yahoo.fr>
2014-01-27 12:24:02 +0100

    refactoring+commentaires à jour

--- Start.rb
+++ Start.rb
@@ -3,51 +3,12 @@
 #=DATE
 #=LICENCE
 
-# Ecran de depart
-#Fonction d'affichage de l'ecran titre
-def start 
-	puts "Type Speed"
-end 
-
-#start
 #variables
 
-time_type=10
+time_char=2  #duree de l'affichage de chaque charactère, lettre ou bit
+time_type=10 #délai maximal avant la saisie d'une lettre
 
-# Affichage regles du jeu
-
-puts "Type Speed est un jeu de réflexes :\n
-	tapez les lettres dès qu'elles apparaissent.\n 
-	Vous diposez de #{time_type} secondes pour saisir chaque lettre.\n
-	La partie est perdue en cas d'erreur de saisie \n
-	ou si le temps est dépassé."
-	
-
-
-#Lancement du jeu - confirmation jouer
-def launch
-	puts "Voulez vous lancer la partie ? [Y/n]"
-	player_choice=gets.chomp
-	if player_choice =="Y" || player_choice == "y" 
-		#launch2
- 	else 
-		puts"test"
- 	end
-end 
-
-
-
-# Jeu
-
-def choice_random 
-choice_function = rand(0..10)
-	if choice_function >= 8
-		return randomLetters
-	else
-		return randomBits
-	end
-end
-
+#fonctions basiques
 
 #Generation de 0 et 1 aleatoires
 def randomBits
@@ -60,15 +21,56 @@ def randomLetters
 	return puts randomLetter=('a'..'z').to_a[rand(0...26)]
 end
 
+#choix aléatoire de l'affichage d'une lettre ou d'un bit (bit dans 80% des cas)
 
-#Boucle Principale
+def choice_random 
+choice_function = rand(0..10)
+	if choice_function >= 8
+		return randomLetters
+	else
+		return randomBits
+	end
+end
+
+
+
+
+# grandes fonctions du jeu
+
+# Ecran de depart / Affichage de l'ecran titre
+def start 
+	puts "Type Speed\n Le jeu"
+end 
+
+# Affichage regles du jeu
+
+def rules
+	puts "Type Speed est un jeu de réflexes :\n
+	tapez les lettres dès qu'elles apparaissent.\n 
+	Vous diposez de #{time_type} secondes pour saisir chaque lettre.\n
+	La partie est perdue en cas d'erreur de saisie \n
+	ou si le temps est dépassé."
+end
+
+
+#Lancement du jeu - confirmation joueur
+def launch
+	puts "Voulez vous lancer la partie ? [Y/n]"
+	player_choice=gets.chomp
+	if player_choice =="Y" || player_choice == "y" 
+		#launch2
+ 	else 
+		puts"test"
+ 	end
+end 
+
+# Ecran de jeu
 
 def main_game
 
 	(0..30).each { |x|
 		
 		if choice_random.is_a?(Integer)
-
 			break
 		else 
 			input_player=gets.chomp 
@@ -81,18 +83,10 @@ def main_game
 		end
 
 		delay 2.0
-
 	}
 end
 
-=begin
-
-30.each do {|i|
-	puts i
-	delay 2s
-}
-endz
-
 #Ecran resultat
-=end
+
+
 
