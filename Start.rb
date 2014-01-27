b9485687c68dbcd04c8769bd043b30653939857f
RemyMaucourt <remy.maucourt@yahoo.fr>
2014-01-27 15:16:50 +0100

    fonctions jeu terminées

--- Start.rb
+++ Start.rb
@@ -3,12 +3,15 @@
 #=DATE
 #=LICENCE
 #--rdoc a faire
-#variables
 
+require "timeout"
 require "pry"
 
-time_char=2  #duree de l'affichage de chaque charactère, lettre ou bit
-time_type=10 #délai maximal avant la saisie d'une lettre
+#variables
+BEGIN{
+TIME_CHAR=2  #duree de l'affichage de chaque charactère, lettre ou bit
+TIME_TYPE=2 #délai maximal avant la saisie d'une lettre
+}
 
 #fonctions basiques
 
@@ -34,37 +37,50 @@ choice_function = rand(0..10)
 	end
 end
 
-
-
-
 # grandes fonctions du jeu
 
 # Ecran de depart / Affichage de l'ecran titre
 def start 
-	puts "Type Speed\n Le jeu"
+	puts "\nType Speed\nLe jeu"
+	sleep 2
+	system("cls")
 end 
 
 # Affichage regles du jeu
 #--Pour le fun faire un dessin ascii
 def rules
-	puts "Type Speed est un jeu de réflexes :\n
-	tapez les lettres dès qu'elles apparaissent.\n 
-	Vous diposez de #{time_type} secondes pour saisir chaque lettre.\n
+	puts"	
+	Type Speed est un jeu de réflexes :\n
+	tapez les lettres dès qu'elles apparaissent,\n
+	ne faites rien quand un chiffre apparaît.\n 
+	Vous diposez de #{TIME_TYPE} secondes pour saisir chaque lettre.\n
 	La partie est perdue en cas d'erreur de saisie \n
 	ou si le temps est dépassé."
+	sleep 5
 end
 
 
 #Lancement du jeu - confirmation joueur
 #--Faire un debug au cas ou l'utilisateur rentre autre
 #--parametre que Y,y, N,n 
+# + compte à rebours lancement
 def launch
-	puts "Voulez vous lancer la partie ? [Y/n]"
+	puts "\nVoulez vous lancer la partie ? [Y/n]"
 	player_choice=gets.chomp
-	if player_choice =="Y" || player_choice == "y" 
-		#launch2
+	if player_choice =="Y" || player_choice == "y"
+		puts"Attention:"
+		sleep 1
+		puts"3..."
+		sleep 1
+		puts"2.."
+		sleep 1
+		puts"1."
+		sleep 1
+		puts"GO !!!"
+		sleep 1
+		main_game
  	else 
-		puts"test"
+		puts"Tant pis pour toi."
  	end
 end 
 
@@ -83,38 +99,38 @@ end
 
 def main_game
 
-	(0..30).each { |x|
+	(0..30).each do |x|
 		
 		a=choice_random
 		puts a
 			if a.kind_of?(Integer)
 		
-			else 
-				input_player=gets.chomp 
+			else
+				begin
+					Timeout::timeout(TIME_TYPE) do
 
-				if input_player == a
-					
-				else
-					gameover
+						input_player=gets.chomp 
+
+						if input_player == a
+							
+						else
+							gameover
+							break
+						end
+
+						end
+
+				rescue Timeout::Error
+						gameover
+						break
+				end
+
 			end
-		end
-		sleep(2.0) 
-	}
+
+		sleep(TIME_CHAR) 
+	end
 end
 
-
-=begin	
-		a=choice_random
-		if a.is_a?(Integer)
-			binding.pry
-			resume
-		else 
-			input_player=gets.chomp 
-
-			if input_player == choice_random
-				resume
-			else
-				exit
-			end
-		end
-=end
+# start 
+# rules
+launch
